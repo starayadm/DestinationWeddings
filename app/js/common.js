d52815dcbf6ac8da034b60d8b6d8841d4031c914
Lily <33253432+starayadm@users.noreply.github.com>
2019-10-14 12:42:52 +0300

    Section-1 is finished

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,5 @@
- // jQuery
- 
-//  $(function() {
+// Lazyload polyfill
 
-	 
-// });
-
-// // Picturefill polyfill
-
-// document.createElement( "picture" );
-
-// // Lazyload polyfill
-
-// /* <picture>
-// 			<source media="/" data-srcset="/">
-// 			<img data-src="/" alt="/" class="lazy">
-// 		</picture> */
-
-// var myLazyLoad = new LazyLoad({
-// 	elements_selector: ".lazy"
-// });
+var lazyLoadInstance = new LazyLoad({
+	elements_selector: ".lazy"
+});
